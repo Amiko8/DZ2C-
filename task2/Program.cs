6170bcdfa170b4911fc0030340218beb9017de00
Amiko8 <119101599+Amiko8@users.noreply.github.com>
2022-12-10 10:46:15 +0300

    Add files via upload

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 2: Напишите программу, которая выводит случайное трёхзначное число 
+//и удаляет вторую цифру этого числа.
+
+Console.WriteLine("Введите число");
+
+int value = int.Parse(Console.ReadLine());
+int digitsCount = (int)Math.Log10(value) + 1; 
+int multiplier = (int)Math.Pow(10, digitsCount - 2); 
+int result = value % multiplier + value / multiplier / 10 * multiplier; 
+
+Console.WriteLine(result); 
