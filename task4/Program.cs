2d7797a390569877d1f09417425425f0d5724890
Amiko8 <119101599+Amiko8@users.noreply.github.com>
2022-12-10 10:47:18 +0300

    Add files via upload

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным. 
+Console.WriteLine("Введите день недели");
+int value = Convert.ToInt32(Console.ReadLine());
+switch (value)
+{
+    case 6:
+    case 7:
+    Console.WriteLine("Выходной");
+    break;
+    default:
+    Console.WriteLine("Будний день");
+    break;
+}
